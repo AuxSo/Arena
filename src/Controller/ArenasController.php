304bf987a473b5e1721e1c8a9807ab1142d5d70a
RomainBtcl <romain.baticle@gmail.com>
2016-10-19 12:53:23 +0200

    TP1

---
+++ src/Controller/ArenasController.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Controller;
+use App\Controller\AppController;
+/**
+ * Personal Controller
+ * User personal interface
+ *
+ */
+class ArenasController  extends AppController
+{
+    public function index()
+    {
+        $this->set('myname', "Romain Baticle");
+    }
+    public function login()
+    {
+
+    }
+    public function fighter()
+    {
+
+    }
+    public function sight()
+    {
+
+    }
+    public function diary()
+    {
+
+    }
+}
